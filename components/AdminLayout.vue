24f1f7eb1d17f1da8165828383e0dee56b02c564
Joseph Levarato <jd.levarato@gmail.com>
2023-07-11 00:27:51 +0200

    Added some pages + links

--- components/AdminLayout.vue
+++ components/AdminLayout.vue
@@ -5,15 +5,33 @@
             <!-- Page content here -->
             <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
-            <slot />
+            <div class="m-4">
+                <slot />
+            </div>
 
         </div>
         <div class="drawer-side bg-base-300">
             <label for="my-drawer-2" class="drawer-overlay"></label>
-            <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            <ul class="menu p-4 w-60 h-full bg-base-200 text-base-content">
                 <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li>
+                    <RouterLink to="/admin">
+                        <BookmarkSquareIcon class="w-4 h-4" />
+                        Roleplays
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/admin/utilisateurs">
+                        <UsersIcon class="w-4 h-4" />
+                        Utilisateurs
+                    </RouterLink>
+                </li>
+                <li>
+                    <a>
+                        <ChatBubbleBottomCenterTextIcon class="w-4 h-4" />
+                        Salons de discussion
+                    </a>
+                </li>
             </ul>
 
         </div>
@@ -21,5 +39,16 @@
 </template>
 
 <script setup lang="ts">
+import {
+    ChatBubbleBottomCenterTextIcon,
+    UsersIcon,
+    BookmarkSquareIcon
+} from '@heroicons/vue/24/solid'
 
 </script>
+
+<style scoped>
+.router-link-exact-active {
+    @apply bg-base-300;
+}
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,6 +1,14 @@
 <template>
     <AdminLayout>
-        <h1>Administration</h1>
+        <div class="card w-96 bg-base-200 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
     </AdminLayout>
 </template>
 
