9113cda13b69f3fc4e178106c91c1bf8b2839a06
Sarah <sarah.boumier01@gmail.com>
2023-08-13 22:31:09 +0200

    toujours en train d'essayer d'afficher un graphique en vain

--- pages/disponibilites.vue
+++ pages/disponibilites.vue
@@ -4,33 +4,54 @@
     <details class="p-5 dropdown mb-32 ml-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-40 focus:outline-none focus:ring-blue-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-400">
   <summary class="ml-100">Selection du Jour</summary>
   <ul class="p-2 shadow menu dropdown-content z-[1] bg-blue-500 rounded-box w-52">
-    <li @click="test()"><span>Lundi</span></li>
-    <li @click="display_availability('mardi')"><span >Mardi</span></li>
-    <li><a>Mercredi</a></li>
-    <li><a>Jeudi</a></li>
-    <li><a>Vendredi</a></li>
-    <li><a>Samedi</a></li>
-    <li><a>Dimanche</a></li>
+    <li><a @click="display_availability('lundi')">Lundi</a></li>
+    <li><a @click="display_availability('mardi')">Mardi</a></li>
+    <li><a @click="display_availability('mercredi')">Mercredi</a></li>
+    <li><a @click="display_availability('mardi')">Jeudi</a></li>
+    <li><a @click="display_availability('lundi')">Vendredi</a></li>
+    <li><a @click="display_availability('mardi')">Samedi</a></li>
+    <li><a @click="display_availability('lundi')">Dimanche</a></li>
   </ul>
-</details>
+    </details>
+    <Doughnut :data="data" :options="options" />
 </div>
-</template>
-<script setup lang="ts">
 
+</template>
+<script setup lang="ts" >
+import Chart from 'chart.js/auto';
+import { ref, onMounted } from 'vue';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import * as chartConfig from './chartConfig.js'
 import type { Database } from '@/types/supabase'
+import { Bar } from 'vue-chartjs'
+import { Doughnut } from 'vue-chartjs'
+import { createApp } from 'vue'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+const data = {
+  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 20, 80, 10]
+    }
+  ]
+}
+const options = { responsive: true,maintainAspectRatio: false}
+
 const supabase = useSupabaseClient<Database>()
 const dayjs = useDayjs()
+let heures = ref(Array.from(Array(48).keys(), (x) => ([x, 0])))
+let isChartMounted = ref(false)
+
     async function display_availability(day : string)
     {
-        console.log("saluit")
         const { data, error }: any = await supabase.from('users').select(`availability`)
-        const falsef = "false"
-        let heures = Array.from(Array(48).keys(), (x) => ([x, 0]))
         const weekday = ["dimanche", "lundi","mardi","mercredi","jeudi","vendredi","samedi"];
         data.forEach((d) =>
         {
             const { availability } = d
-            console.log(d.availability.available)
+           // console.log(d.availability.available)
             availability.available.forEach((available) =>
             {
                 if (available.isSpecific === false)
@@ -45,7 +66,7 @@ const dayjs = useDayjs()
                         resultat2 = 48;
                         while (resultat < resultat2)
                         {
-                            heures[resultat][1] += 1;
+                            heures.value[resultat][1] += 1;
                             resultat++;
                         }
                     }
@@ -61,11 +82,10 @@ const dayjs = useDayjs()
             {
                 const ds = unavailable.begin
                 const end = unavailable.end
-                console.log(unavailable.begin)
                 check_date(ds, end, weekday,2,day,heures)
             })
         });
-        console.log(heures)
+        isChartMounted.value = true;
     }
     function thirty_or_zero(minute : Number, date : date)
     {
@@ -75,10 +95,6 @@ const dayjs = useDayjs()
             date = date - minute * 60000
             return (date)
     }
-    function test()
-    {
-        console.log("hihi")
-    }
     function check_date(ds: any, end: any, weekday: array, n : number, day: string, heures: array)
     {
       
@@ -87,7 +103,7 @@ const dayjs = useDayjs()
         datenow = datenow.getTime()
         const msremaining = (datenow % 86400000) + 7200000
         let date2 = new Date(ds)
-        console.log("date2 : " + date2)
+       // console.log("date2 : " + date2)
         let date3 = new Date(end)
         let tmp = datenow  - date2
         tmp = Math.trunc(tmp/86400000);
@@ -101,23 +117,52 @@ const dayjs = useDayjs()
         let resultat3 = 604800000 - (86400000 + msremaining)
         if (((date2 - datenow) > 0)  && (((date2 - datenow ) < (604800000 - (86400000 + msremaining))) && (dayofweek === day)))
         {
-            console.log("n = " + n)
+            //console.log("n = " + n)
             let hourb = (((date2 % 86400000)/3600000) + 2) * 2
             let houre = (((date3 % 86400000)/3600000) + 2) * 2
-            console.log(hourb + "--- " + houre)
+            //console.log(hourb + "--- " + houre)
 
             while (hourb < houre)
             {
                 if (n === 1)
-                heures[hourb][1] += 1;
+                heures.value[hourb][1] += 1;
                 else
                 {
-                    console.log("avant " + heures[hourb][1])
-                    heures[hourb][1] -= 1;
-                    console.log("après " + heures[hourb][1])
+                    //console.log("avant " + heures[hourb][1])
+                    heures.value[hourb][1] -= 1;
+                    //console.log("après " + heures[hourb][1])
                 }
                 hourb++;
             }
         }
     }
+    onMounted(() => 
+        {
+            if (isChartMounted.value === true)
+            {
+                const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+                const myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ['00h00', '00h30', '1h00', '1h30', '2h00', '02h30', '3h00', '3h30', '4h00', '4h30','05h00', '05h30', '6h00', '6h30', '7h00','07h30', '08h00', '8h30', '9h00', '9h30','10h00', '10h30', '11h00', '11h30', '12h00','12h30', '13h00', '13h30', '14h00', '14h30','15h00', '15h30', '16h00', '16h30', '17h00','17h30', '18h00', '18h30', '19h00', '19h30','20h00', '20h30', '21h00', '21h30', '22h00','22h30', '23h30'],
+                        datasets: [{
+                            label: 'Nombre de personne disponible selon l\'heure',
+                            data: [heures.value[0][1], heures.value[1][1], heures.value[0][1], heures.value[1][1], heures.value[2][1], heures.value[3][1],heures.value[4][1], heures.value[5][1], heures.value[6][1],heures.value[7][1], heures.value[8][1], heures.value[9][1], heures.value[10][1],heures.value[11][1], heures.value[12][1], heures.value[13][1],heures.value[14][1], heures.value[15][1], heures.value[16][1], heures.value[17][1],heures.value[18][1], heures.value[19][1], heures.value[20][1],heures.value[21][1], heures.value[22][1], heures.value[23][1], heures.value[24][1],heures.value[25][1], heures.value[26][1], heures.value[27][1],heures.value[28][1], heures.value[29][1], heures.value[30][1], heures.value[31][1],heures.value[32][1], heures.value[33][1], heures.value[34][1],heures.value[35][1],heures.value[36][1], heures.value[37][1], heures.value[38][1],heures.value[39][1],heures.value[40][1], heures.value[41][1], heures.value[42][1],heures.value[43][1], heures.value[44][1], heures.value[45][1],heures.value[46][1],heures.value[47][1]],
+                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            borderWidth: 1,
+                        }],
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true,
+                            },
+                        },
+                    },
+                });
+            }
+        });
+    
+   
 </script>
