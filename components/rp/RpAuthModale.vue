09212281763d7c55916e1e5d614d7ca1331fa1f4
josephlevarato <joseph@idusante.fr>
2023-12-15 01:56:30 +0100

    WIP in progress

---
+++ components/rp/RpAuthModale.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const props = defineProps<{ show: boolean }>()
+
+const emit = defineEmits<{
+    (e: 'update:show', value: boolean): void
+}>()
+const { t } = useI18n()
+
+const action = ref<'login' | 'register'>('login')
+const loading = ref(false)
+
+const showProxy = computed({
+    get: () => props.show,
+    set: value => emit('update:show', value),
+})
+
+function authSuccessful() {
+    showProxy.value = false
+}
+</script>
+
+<template>
+    <VDialog
+        v-model="showProxy"
+        :persistent="true"
+        width="700"
+    >
+        <VCard
+            prepend-icon="mdi-account-question"
+            :loading="loading"
+        >
+            <template #title>
+                {{ t('common.login') }}
+            </template>
+            <template #text>
+                <VTabs
+                    v-model="action"
+                    :grow="true"
+                >
+                    <VTab
+                        value="login"
+                    >
+                        {{ t('login.action') }}
+                    </VTab>
+                    <VTab
+                        value="register"
+                    >
+                        {{ t('register.action') }}
+                    </VTab>
+                </VTabs>
+                <VWindow
+                    v-model="action"
+                >
+                    <VWindowItem value="login">
+                        <RpAuthLogin
+                            v-model:loading="loading"
+                            @login="authSuccessful"
+                        />
+                    </VWindowItem>
+                    <VWindowItem value="register">
+                        <RpAuthRegister
+                            v-model:loading="loading"
+                            @register="authSuccessful"
+                        />
+                    </VWindowItem>
+                </VWindow>
+            </template>
+        </VCard>
+    </VDialog>
+</template>
