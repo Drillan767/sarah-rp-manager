45091eb1aaab67e651910c1128b4c6ec48536109
Drillan767 <jd.levarato@gmail.com>
2024-07-19 14:14:49 +0200

    Started cleanup parent + middleware, tests not done yet

--- middleware/authControl.global.ts
+++ middleware/authControl.global.ts
@@ -2,60 +2,19 @@ import type { Database } from '~/types/supabase'
 import type { CurrentUser } from '~/types/models'
 
 export default defineNuxtRouteMiddleware(async (to) => {
+    if (!to.name)
+        return
+
     const currentUser = useState<CurrentUser | undefined>('current-user')
 
-    if (to.name && to.name.toString() === '/roleplays/create' && !currentUser.value)
+    const protectedRoutes = [
+        'roleplays-rpId-channels',
+        'roleplays-rpId-edit',
+        'roleplays-create',
+        'my-roleplays',
+        'profile',
+    ]
+
+    if (protectedRoutes.some(r => to.name?.toString().startsWith(r)) && !currentUser.value)
         return navigateTo('/')
-    // if (to.name && /roleplays-id-(characters|edit).*/.test(to.name.toString())) {
-
-    /* const supabase = useSupabaseClient<Database>()
-        const supabaseUser = useSupabaseUser()
-        let rpFound: any = null
-
-        if (!supabaseUser.value)
-            return navigateTo(`/roleplays/${to.params.id}`)
-
-        const userId = supabaseUser.value.id
-
-        const { data: currentUser } = await supabase
-            .from('users')
-            .select('id')
-            .eq('session_id', userId)
-            .single()
-
-        if (to.name.toString().includes('characters')) {
-            const { data } = await supabase
-                .from('roleplays')
-                .select(`
-                    id,
-                    roles(
-                        id,
-                        characters(
-                            status
-                        )
-                    )
-                `)
-                .or(`user_id.eq.${currentUser!.id}, and(user_id.eq.${currentUser!.id},status.eq.2)`, { foreignTable: 'roles.characters' })
-
-            rpFound = data ?? null
-        }
-
-        if (to.name.toString().includes('edit')) {
-            const { data } = await supabase
-                .from('roleplays')
-                .select('user_id')
-                .eq('user_id', currentUser!.id)
-
-            rpFound = data ?? null
-        }
-
-        if (!rpFound)
-            return navigateTo(`/roleplays/${to.params.id}`)
-    }
-
-    if (to.name && to.name === '/my-roleplays') {
-        const supabaseUser = useSupabaseUser()
-        if (!supabaseUser.value)
-            return navigateTo('/')
-    } */
 })
