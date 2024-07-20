26f38292cd52f06e242c7bf9a3ebdc4f35396217
Drillan767 <jd.levarato@gmail.com>
2024-07-20 22:32:18 +0200

    progress

---
+++ components/layout/NavBarMenu.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { useDisplay, useTheme } from 'vuetify'
+import type { Database } from '~/types/supabase'
+import type { CurrentUser } from '~/types/models'
+
+defineProps<{ displayUsername: boolean }>()
+
+const currentUser = useState<CurrentUser | undefined>('current-user')
+
+const { t } = useI18n()
+const theme = useTheme()
+const { mobile } = useDisplay()
+
+const config = useRuntimeConfig()
+const supabase = useSupabaseClient<Database>()
+const router = useRouter()
+const user = useSupabaseUser()
+
+async function logout() {
+    currentUser.value = undefined
+    await supabase.auth.signOut()
+    await router.push('/')
+}
+
+function switchTheme() {
+    theme.global.name.value = theme.global.current.value.dark
+        ? 'light'
+        : 'dark'
+}
+
+async function login() {
+    await supabase.auth.signInWithOAuth({
+        provider: 'twitter',
+        options: {
+            redirectTo: config.public.redirectUrl,
+        },
+    })
+}
+
+watch(user, (value) => {
+    if (value) {
+        const { user_metadata: twitter } = value
+
+        currentUser.value = {
+            id: value.id,
+            handle: twitter.user_name,
+            username: twitter.name,
+            avatar: twitter.picture,
+        }
+    }
+}, { immediate: true })
+</script>
+
+<template>
+    <template v-if="currentUser">
+        <VListItem
+            v-if="!mobile && displayUsername"
+            :title="currentUser.username"
+            :subtitle="`@${currentUser.handle}`"
+            class="text-right"
+        />
+
+        <VMenu anchor="bottom end">
+            <template #activator="{ props }">
+                <VBtn v-bind="props" icon>
+                    <VAvatar :image="currentUser.avatar" />
+                </VBtn>
+            </template>
+            <VList>
+                <VListItem
+                    v-if="mobile"
+                    :title="currentUser.username"
+                    :subtitle="`@${currentUser.handle}`"
+                    class="text"
+                />
+                <VDivider v-if="mobile" />
+                <VListItem
+                    to="/profile"
+                    prepend-icon="mdi-account"
+                    :title="t('account.self')"
+                />
+                <VListItem
+                    to="/my-roleplays"
+                    prepend-icon="mdi-book-open-page-variant-outline"
+                    :title="t('pages.roleplays.my')"
+                />
+                <VDivider />
+                <VListItem
+                    prepend-icon="mdi-logout"
+                    :title="t('account.logout')"
+                    @click="logout"
+                />
+                <VDivider />
+                <VListItem
+                    :prepend-icon="
+                        theme.current.value.dark
+                            ? 'mdi-white-balance-sunny'
+                            : 'mdi-weather-night'
+                    "
+                    :title="
+                        t(
+                            theme.current.value.dark
+                                ? 'account.light'
+                                : 'account.dark',
+                        )
+                    "
+                    @click="switchTheme"
+                />
+            </VList>
+        </VMenu>
+    </template>
+
+    <VBtn v-else color="primary" @click="login">
+        {{ t('login.action') }}
+    </VBtn>
+</template>
--- pages/roleplays/[rpId]/channels.vue
+++ pages/roleplays/[rpId]/channels.vue
@@ -1,13 +1,54 @@
 <script setup lang="ts">
-import type { CurrentUser } from '@/types/models'
-import RPLayoutVue from '~/components/rp/RPLayout.vue.vue'
+import type { Channel, CurrentUser, Roleplay } from '@/types/models'
+import type { Database } from '~/types/supabase'
+import RPLayout from '~/components/rp/RPLayout.vue.vue'
 
+const supabase = useSupabaseClient<Database>()
 const user = useSupabaseUser()
 const currentUser = useState<CurrentUser | undefined>()
+const route = useRoute()
+
+const roleplayId = ref('')
+const roleplay = ref<Roleplay>()
+const channels = ref<Channel[]>([])
+const roleplayLoading = ref(false)
+const channelsLoading = ref(false)
 
 definePageMeta({
     layout: 'channels',
 })
+
+useHead({
+    title: () => roleplay.value?.title ?? '',
+})
+
+async function loadRoleplay() {
+    roleplayLoading.value = true
+    const { data } = await supabase
+        .from('roleplays')
+        .select('*, roles(*)')
+        .eq('id', roleplayId.value)
+        .single()
+
+    if (data)
+        roleplay.value = data
+
+    roleplayLoading.value = false
+}
+
+async function loadChannels() {
+// Initial fetch for the channels.
+    channelsLoading.value = true
+    const { data } = await supabase
+        .from('channels')
+        .select('*')
+        .eq('roleplay_id', roleplayId.value)
+
+    if (data)
+        channels.value = data
+
+    channelsLoading.value = false
+}
 
 watch(user, (value) => {
     if (value) {
@@ -21,17 +62,42 @@ watch(user, (value) => {
         }
     }
 }, { immediate: true })
+
+watch(() => route.params, ({ rpId }) => {
+    roleplayId.value = rpId.toString()
+    loadRoleplay()
+    loadChannels()
+}, { immediate: true })
 </script>
 
 <template>
-    <RPLayoutVue>
+    <RPLayout
+        :roleplay="roleplay"
+        :loading="roleplayLoading"
+    >
         <template #channels>
-            <p>Channels</p>
+            <VSkeletonLoader
+                :loading="channelsLoading"
+                type="list-item-avatar-two-line@2"
+            >
+                <VList class="w-100">
+                    <VListSubheader
+                        title="Canaux de discussion"
+                    />
+                    <VListItem
+                        v-for="(channel, i) in channels"
+                        :key="i"
+                        :prepend-icon="channel.internal ? 'mdi-chat-outline' : 'mdi-chat'"
+                        :title="channel.name"
+                        :to="`/roleplays/${roleplayId}/channels/${channel.id}`"
+                    />
+                </VList>
+            </VSkeletonLoader>
         </template>
         <template #characters>
             <p>Characters</p>
         </template>
-    </RPLayoutVue>
+    </RPLayout>
 
     <NuxtPage />
 </template>
