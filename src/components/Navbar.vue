68d0cdd179d1f3f3227c485f2cca491b5b0a6441
Drillan767 <jd.levarato@gmail.com>
2025-05-25 00:24:08 +0200

    Main + navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import NavBarMenu from './NavbarMenu.vue'
+</script>
+
+<template>
+    <VAppBar
+        title="Le Jardin de Sarah"
+        class="px-6"
+    >
+        <template #prepend>
+            <RouterLink to="/">
+                <VAvatar :tile="true" image="/sarah.jpg" />
+            </RouterLink>
+        </template>
+        <template #append>
+            <NavBarMenu :display-username="true" />
+        </template>
+    </VAppBar>
+</template>
